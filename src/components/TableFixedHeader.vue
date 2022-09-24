dd6db2ef0978e579e9dab571f4718960c2e852b5
Alexander Vanlancker <alexander.vanlancker@codifly.be>
2022-09-24 23:55:51 +0200

    vuetify setup

---
+++ src/components/TableFixedHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-table fixed-header height="300px">
+    <thead>
+      <tr>
+        <th class="text-left">Name</th>
+        <th class="text-left">Calories</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in items" :key="item.name">
+        <td>{{ item }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  items: Array<any>;
+}
+defineProps<Props>();
+console.log();
+</script>
+
+<style lang=""></style>
--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -3,24 +3,30 @@
     <!-- <p v-for="product in products" :key="product.id">
       <CardItem :name="product.name" :price="product.price" />
     </p> -->
-    <Card></Card>
+    <v-btn color="primary"> Normal Button </v-btn>
     <p>test</p>
+    <Suspense>
+      <TableFixedHeader :items="customers" />
+    </Suspense>
   </main>
 </template>
 
 <script setup lang="ts">
 import CardItem from "@/components/CardItem.vue";
 import axios from "axios";
+import TableFixedHeader from "../components/TableFixedHeader.vue";
 
 interface Product {
   id: number;
   name: string;
   price: number;
 }
-
-// const products = productsResponse.data;
 // const productsResponse = await axios.get<Array<Product>>(
 //   "http://localhost:4249/api/Product"
 // );
 // const products = productsResponse.data;
+const customersResponse = await axios.get<Array<Product>>(
+  "https://localhost:7107/api/Customer"
+);
+const customers: Array<any> = customersResponse.data;
 </script>
